2c3abc4e79ee6e638b62d5e87714dd13ac08d7ee
StellaSFE96 <stella.samaranayake@iths.se>
2024-07-30 20:03:47 +0200

    remove .env file try 2

--- src/API/api.ts
+++ src/API/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'your_api_key';
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const fetchMovies = (query: string) => {
