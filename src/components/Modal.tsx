0140069cc3d87f1e4a4b5c8c1db10c18b10c9cdf
StellaSFE96 <stella.samaranayake@iths.se>
2024-08-02 00:11:02 +0200

    styled modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { Movie } from '../types/movieTypes';
-import MovieCard from './MovieCard';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Movie } from "../types/movieTypes";
+import MovieCard from "./MovieCard";
 
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
-  listType: 'favorites' | 'toWatch';
+  listType: "favorites" | "toWatch";
 }
 
 const Overlay = styled.div`
@@ -19,17 +19,27 @@ const Overlay = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  z-index: 1000;
 `;
 
 const ModalContent = styled.div`
   background: white;
   padding: 20px;
-  border-radius: 8px;
-  max-width: 500px;
-  width: 100%;
-  max-height: 80vh;
+  border-radius: 10px;
+  max-width: 1000px;
+  width: 60%;
+  max-height: 85vh;
   overflow-y: auto;
   position: relative;
+  z-index: 1001;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+  flex-wrap: wrap;
 `;
 
 const CloseButton = styled.button`
@@ -40,6 +50,13 @@ const CloseButton = styled.button`
   top: 10px;
   right: 10px;
   cursor: pointer;
+`;
+
+const Title = styled.h2`
+  color: black;
+`;
+const Message = styled.p`
+  color: black;
 `;
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, listType }) => {
@@ -57,18 +74,30 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, listType }) => {
     return null;
   }
 
+  const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+
   return (
-    <Overlay>
-      <ModalContent>
+    <Overlay onClick={handleOverlayClick}>
+      <ModalContent onClick={(e) => e.stopPropagation()}>
         <CloseButton onClick={onClose}>&times;</CloseButton>
-        <h2>{listType === 'favorites' ? 'Favorite Movies' : 'To Watch Movies'}</h2>
-        {movies.length > 0 ? (
-          movies.map(movie => (
-            <MovieCard key={movie.id} movie={movie} />
-          ))
-        ) : (
-          <p>No {listType === 'favorites' ? 'favorite' : 'to watch'} movies yet.</p>
-        )}
+        <Title>
+          {listType === "favorites" ? "Favorite Movies" : "Movies to Watch"}
+        </Title>
+        <Content>
+          {movies.length > 0 ? (
+            movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
+          ) : (
+            <Message>
+              No {listType === "favorites" ? "favorite" : "to watch"} movies
+              yet.
+            </Message>
+          )}
+        </Content>
       </ModalContent>
     </Overlay>
   );
